4eeeb804bbb12221961c6a31e2d05a97389f0c56
KAYSILVHA <kaysilvha@gmail.com>
2023-07-18 23:29:20 -0300

    add media queri

--- src/home/style.js
+++ src/home/style.js
@@ -3,10 +3,16 @@ import styled from "styled-components";
 export const Paragrafo = styled.p`
 color: red;
 font-size: 25px;
+&:hover{
+  color: blue;
+} 
 `
-
 
 export const HeaderNav = styled.header`
 background-color: black;
 color: white;
+
+@media screen and (max-width: 500px){
+  background-color: darkcyan;
+}
 `
